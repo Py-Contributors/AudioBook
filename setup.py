f0b57fe475e9ddaa3adfb4bd4a8436faf4121fee
Deepak Raj <deepak008@live.com>
2020-10-22 18:02:57 +0530

    initial commit

--- setup.py
+++ setup.py
@@ -6,32 +6,30 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     #Here is the module name.
     name="audiobook",
- 
     #version of the module
-    version="0.0.1",
- 
+    version="1.0.0",
     #Name of Author
     author="CodePerfectPlus",
- 
     #your Email address
     author_email="deepak008@live.com",
- 
     #Small Description about module
     description="listen your favourite audiobook",
- 
     long_description=long_description,
- 
     #Specifying that we are using markdown file for description
     long_description_content_type="text/markdown",
- 
     #Any link to reach this module, if you have any webpage or github profile
     url="https://github.com/codePerfectPlus/audiobook",
     packages=setuptools.find_packages(),
- 
     #classifiers like program is suitable for python3, just leave as it is.
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Developers',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: Linux :: Linux',
+
     ],
 )
---
+++ tests/test.py
@@ -0,0 +1,4 @@
+from audiobook import Audiobook
+
+ab = Audiobook("/home/codeperfectplus/Documents/Books/learningopencv.pdf")
+ab.text_to_speech()
