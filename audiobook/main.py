e3dc0dc0bd9f7a2be8da7969e9a040fe33cadc76
Deepak Raj <54245038+codePerfectPlus@users.noreply.github.com>
2022-10-13 15:02:07 +0530

    :hammer: flake8 lint

--- audiobook/main.py
+++ audiobook/main.py
@@ -6,10 +6,11 @@ speed_dict = {
     "normal": 150,
     "fast": 200}
 
+
 def speak_text(engine, text):
     engine.say(text)
     engine.runAndWait()
-    
+
 
 class AudioBook:
     def __init__(self, speaker=None, speed="medium"):
@@ -21,16 +22,14 @@ class AudioBook:
         with open(book_path, "rb") as fp:
             pdfReader = PyPDF2.PdfFileReader(fp)
             pages = pdfReader.numPages
-            speak_text(self.engine, "The Book has total: " + str(pages) + " pages!")
+            speak_text(self.engine, f"The book has total {str(pages)} pages!")
             speak_text(self.engine, "Please enter the page number: ")
-            
+
             start_page = int(input("Please enter the page number: "))
             speak_text(self.engine, "Reading the book now!")
-            
+
             for num in range(start_page, pages):
                 speak_text(self.engine, "Reading page number " + str(num))
                 page = pdfReader.getPage(num)
                 text = page.extractText()
                 speak_text(self.engine, text)
-
-    
