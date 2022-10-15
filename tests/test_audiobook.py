d549e5667dc6af0caf90d58529ada0cac9e890da
FlowerOfTheBridges <gfiordeponti@gmail.com>
2022-10-15 23:42:33 +0200

    Fix test

--- tests/test_audiobook.py
+++ tests/test_audiobook.py
@@ -1,25 +1,25 @@
 from audiobook import AudioBook
 import unittest
-from PyPDF2.utils import PdfReadError
+from PyPDF2.errors import PdfReadError
 
 
 class TestAudioBook(unittest.TestCase):
     def test_invalidPathNumeric(self):
         with self.assertRaises(IOError):
-            ab = AudioBook(123)
-            ab.text_to_speech()
+            ab = AudioBook('normal')
+            ab.txt_to_json(123)
 
     def test_openDirectory(self):
         with self.assertRaises(IsADirectoryError):
-            ab = AudioBook('/')
-            ab.text_to_speech()
+            ab = AudioBook('normal')
+            ab.txt_to_json('/')
 
     def test_fileDoesNotExist(self):
         with self.assertRaises(FileNotFoundError):
-            ab = AudioBook('oiawhgaiurgieurghergerg')
-            ab.text_to_speech()
+            ab = AudioBook('normal')
+            ab.txt_to_json('oiawhgaiurgieurghergerg')
 
     def test_fileIsNotPDF(self):
         with self.assertRaises(PdfReadError):
-            ab = AudioBook(__file__)
-            ab.text_to_speech()
+            ab = AudioBook('normal')
+            ab.pdf_to_json(__file__)
