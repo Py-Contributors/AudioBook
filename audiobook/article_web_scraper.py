cc99eb93d3574a7a3d89c9b0db8c39c9ebf5a083
FlowerOfTheBridges <gfiordeponti@gmail.com>
2022-10-16 13:48:51 +0200

    Add support for pagination in read/store article

--- audiobook/article_web_scraper.py
+++ audiobook/article_web_scraper.py
@@ -9,7 +9,8 @@ class ArticleWebScraper:
     ArticleWebScraper class
     
     methods:
-        get_text_lines_from_web_article:
+        get_json_from_web_article: returns a json from a non-empty <article> tag
+        get_title_from_article: returns the <title> tag from the html page
         
     sample usage:
         ab = AudioBook(speed="normal")
@@ -22,20 +23,28 @@ class ArticleWebScraper:
         self.soup = BeautifulSoup(page.content, "html.parser")
 
     def get_title_from_article (self):
+        """ returns the <title> tag from the html page """
         return self.soup.title.text
     
-    def get_text_lines_from_web_article (self):
+    def get_json_from_web_article (self):
+        """ returns a json from a non-empty <article> tag """
         if hasattr(self.soup, 'article') and self.soup.article is not None: 
             article_text_tag_items = [
                 self.soup.article.findChildren(text_formatting , recursive=True) 
                 for text_formatting in html_text_formattings
             ]
+
+            json_article = {}
             text_lines = []
-            # dict.keys(dict.fromkeys(lines))) removes duplicate words
+            # list(dict.fromkeys(lines))) removes duplicate words in same tag type
             for article_text_tag_item in article_text_tag_items:
                 for article_text_tag in article_text_tag_item:
-                    text_line = dict.keys(dict.fromkeys([tag.string for tag in article_text_tag if tag.string is not None])) 
+                    text_line = list(dict.fromkeys([tag.string for tag in article_text_tag if tag.string is not None])) 
                     text_lines += text_line
-            return dict.keys(dict.fromkeys(text_lines))
+            # list(dict.fromkeys(lines))) removes duplicate words among all tags
+            text_lines = list(dict.fromkeys(text_lines))
+            for num in range(0, len(text_lines)):
+                json_article[num] = text_lines[num]
+            return json_article, len(json_article)
         else:
             raise ValueError(f"<article> tag not found in {self.article_url}")
--- audiobook/main.py
+++ audiobook/main.py
@@ -32,10 +32,12 @@ class AudioBook:
         file_check: checks if file exists
         pdf_to_json: converts pdf to json format
         create_json_book: Creates json book from input file by calling respective method
-        save_audio: saves audio files in folder
+        read_json: reads a json file
+        save_json_to_audio: save .mp3 audios from a json file in a folder
+        save_book_audio: saves audio files in folder
         read_book: reads the book
         read_web_article: read web article from a given url
-        save_web_article: save web article to a .mp3 file from a given url
+        save_web_article_audio: save web article to a .mp3 file from a given url
         
     sample usage:
         ab = AudioBook(speed="normal")
@@ -93,27 +95,27 @@ class AudioBook:
         return json_book, pages
     
 
-    def save_audio(self, input_file_path, password=None):
+    def save_book_audio(self, input_file_path, password=None):
         """ method to save audio files in folder """
         self.file_check(input_file_path)
         logging.info("Creating your audiobook... Please wait...")
-        json_book, pages = self.create_json_book(input_file_path, password)
+        json_book, _ = self.create_json_book(input_file_path, password)
         
         book_name = os.path.basename(input_file_path).split(".")[0]
-        os.makedirs(book_name, exist_ok=True)
-        logging.info('Saving audio files in folder: {}'.format(book_name))
+        self.save_json_to_audio(json_book, book_name)
         
-        for page_num, text in json_book.items():
-            self.engine.save_to_file(text, os.path.join(book_name, book_name + "_page_" + (str(page_num+1) + ".mp3")))
-            self.engine.runAndWait()
                 
     
     def read_book(self, input_file_path, password=None):
         """ method to read the book """
         self.file_check(input_file_path)
-        logging.info("Creating your audiobook... Please wait...")
+        logging.info(f"Creating your {type}... Please wait...")
         json_book, pages = self.create_json_book(input_file_path, password)
-        speak_text(self.engine, f"The book has total {str(pages)} pages!")
+        self.read_json(json_book, pages, "audiobook")
+        
+    def read_json(self, json, pages, type):
+        """ reads each page of a json"""
+        speak_text(self.engine, f"The {type} has total {str(pages)} pages!")
         speak_text(self.engine, "Please enter the page number: ", display=False)
         start_page = int(input("Please enter the page number: ")) - 1
                         
@@ -121,11 +123,11 @@ class AudioBook:
         while reading:
             if start_page > pages or start_page < 0:
                 speak_text(self.engine, "Invalid page number!")
-                speak_text(self.engine, f"The book has total {str(pages)} pages!")
+                speak_text(self.engine, f"The {type} has total {str(pages)} pages!")
                 start_page = int(input("Please enter the page number: "))
 
             speak_text(self.engine, f"Reading page {str(start_page+1)}")
-            pageText = json_book[start_page]
+            pageText = json[start_page]
             speak_text(self.engine, pageText, display=False)
 
             user_input = input("Please Select an option: \n 1. Type 'r' to read again: \n 2. Type 'p' to read previous page\n 3. Type 'n' to read next page\n 4. Type 'q' to quit:\n 5. Type page number to read that page:\n")
@@ -141,7 +143,7 @@ class AudioBook:
                 start_page += 1
                 continue
             elif user_input == "q":
-                speak_text(self.engine, "Quitting the book!")
+                speak_text(self.engine, f"Quitting the {type}!")
                 break
             elif user_input.isnumeric():
                 start_page = int(user_input) - 1
@@ -149,29 +151,35 @@ class AudioBook:
                 user_input = input("Please Select an option: \n 1. Type 'r' to read again: \n 2. Type 'p' to read previous page\n 3. Type 'n' to read next page\n 4. Type 'q' to quit:\n 5. Type page number to read that page:\n")
                 continue
 
+    def save_json_to_audio(self, json, audio_name):
+        """ save json to a list of file in a folder having audio_name, one for each page """
+        os.makedirs(audio_name, exist_ok=True)
+        logging.info('Saving audio files in folder: {}'.format(audio_name))
+        print(json)
+        for page_num, text in json.items():
+            self.engine.save_to_file(text, os.path.join(audio_name, audio_name + "_page_" + (str(page_num+1) + ".mp3")))
+            self.engine.runAndWait()
+
     def read_web_article(self, article_url):
         """ read web article from a article_url containing an <article> tag """
         ws = ArticleWebScraper(article_url)
-        text_lines = ws.get_text_lines_from_web_article()
-        if len(text_lines) > 0:
-            logger.info(f'reading {len(text_lines)} from {article_url}')
-            speak_text(self.engine, ws.get_title_from_article(), display=False)
-            speak_text(self.engine, ''.join(text_lines), display=False)
+        json_article, pages = ws.get_json_from_web_article()
+        if len(json_article) > 0:
+            self.read_json(json_article, pages, "article")
         else:
             raise ValueError("<article> tag has no text.")
     
-    def save_web_article(self, article_url):
+    def save_web_article_audio(self, article_url):
         """ save web article from a article_url containing an <article> tag """
         ws = ArticleWebScraper(article_url)
-        text_lines = ws.get_text_lines_from_web_article()
-        if len(text_lines) > 0:
+        json_article, _ = ws.get_json_from_web_article()
+        if len(json_article) > 0:
             title = ws.get_title_from_article()
-            mp3_file = os.path.join(os.getcwd(), f"{''.join(letter for letter in title if letter.isalnum())}.mp3")
-            logger.info(f'saving {article_url} to {mp3_file}')
-            self.engine.save_to_file(title+"".join(text_lines), mp3_file)
-            self.engine.runAndWait()
+            current_dir = os.getcwd()
+            folder_name = input(f"Choose name for article \"{title}\". It will be stored in {current_dir}\n")
+            self.save_json_to_audio(json_article, folder_name)
         else:
-            raise ValueError("<article> tag has no text.")
+            raise ValueError("<article> tag is empty.")
         
 
         
